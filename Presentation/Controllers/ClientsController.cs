524e23fd4751bc6251f459ae3c875f3feed4e8fe
PetraElgemyr <petra.elgemyr@gmail.com>
2025-04-02 18:16:55 +0200

    added frontend client modal and styling fixes

--- Presentation/Controllers/ClientsController.cs
+++ Presentation/Controllers/ClientsController.cs
@@ -1,14 +1,51 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Business.Models;
+using Business.Services;
+using Domain.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
 
 namespace Presentation.Controllers;
 
-[Authorize(Roles = "Admin")]
-public class ClientsController : Controller
+//[Authorize(Roles = "Admin")]
+public class ClientsController(IClientService clientService) : Controller
 {
+
+    private readonly IClientService _clientService = clientService;
+
     [Route("admin/clients")]
     public IActionResult Index()
     {
+        ViewBag.ErrorMessage = "";
         return View();
     }
+
+    [HttpPost]
+    [Route("admin/clients")]
+    public async Task<IActionResult> AddClient(AddClientViewModel model)
+    {
+        ViewBag.ErrorMessage = null;
+
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState.Where(x => x.Value?.Errors.Count > 0)
+                .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value?.Errors.Select(e => e.ErrorMessage).ToArray()
+                );
+
+            return BadRequest(new { errors });
+        }
+        var clientFormData = model.MapTo<AddClientFormData>();
+
+        var result = await _clientService.CreateClientAsync(clientFormData);
+        return result.StatusCode switch
+        {
+            200 => Ok(),
+            400 => BadRequest(result.Error),
+            409 => Conflict(),
+            _ => Problem(),
+        };
+    }
+
 }
---
+++ Presentation/Models/AddClientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Models;
+
+public class AddClientViewModel
+{
+    [Required(ErrorMessage = "is required.")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Client Name", Prompt = "Enter client name")]
+    public string ClientName { get; set; } = null!;
+
+    [DataType(DataType.Text)]
+    public string? Image { get; set; }
+}
