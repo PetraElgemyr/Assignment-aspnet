fa5c85125c029e4b452e2c514ba84988fa936a4a
PetraElgemyr <petra.elgemyr@gmail.com>
2025-04-05 22:09:06 +0200

    add view for members (users controller)

--- Presentation/Controllers/UsersController.cs
+++ Presentation/Controllers/UsersController.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Business.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
 
 namespace Presentation.Controllers;
 
 [Authorize(Roles = "Admin")]
-public class UsersController : Controller
+public class UsersController(IUserService userService) : Controller
 {
+
+    private readonly IUserService _userService = userService;
+
     [Route("admin/members")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        ViewBag.ErrorMessage = "";
+
+        var userResults = await _userService.GetUsersAsync();
+
+        if (userResults.Succeeded)
+        {
+            var model = new MembersViewModel
+            {
+                Members = userResults.Result!
+            };
+            return View(model);
+        }
+
+
         return View();
     }
 }
---
+++ Presentation/Models/MembersViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Presentation.Models;
+
+public class MembersViewModel
+{
+    public IEnumerable<User> Members { get; set; } = [];
+}
