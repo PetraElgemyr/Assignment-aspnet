9b9e6da41b5ca01057276e126347d8cf19b60d00
PetraElgemyr <petra.elgemyr@gmail.com>
2025-04-08 20:46:31 +0200

    add local image handler and add google icon on login

---
+++ Business/Handlers/LocalImageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Handlers;
+
+public interface IImageHandler
+{
+    Task<string?> SaveProjectImageAsync(IFormFile file);
+}
+
+public class LocalImageHandler(string imagePath) : IImageHandler
+{
+    private readonly string _imagePath = imagePath;
+
+    public async Task<string?> SaveProjectImageAsync(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return null!;
+
+        var extension = Path.GetExtension(file.FileName);
+        var fileName = $"{Guid.NewGuid()}{extension}";
+
+        if (!Directory.Exists(_imagePath))
+            Directory.CreateDirectory(_imagePath);
+
+        var filePath = Path.Combine(_imagePath, fileName);
+        using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return fileName;
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -2,10 +2,21 @@ using Microsoft.AspNetCore.Rewrite;
 using Business;
 using Hubs;
 using Data.Extensions;
+using Business.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
+
+
+// testa i dev om bildhantering funkar. 
+if (builder.Environment.IsDevelopment())
+{
+    var localPath = Path.Combine(builder.Environment.WebRootPath, "images", "uploads");
+    builder.Services.AddScoped<IImageHandler>(_ => new LocalImageHandler(localPath));
+}
+
+
 
 builder.Services.AddContexts(builder.Configuration.GetConnectionString("SqlConnection")!);
 builder.Services.AddLocalIdentity(builder.Configuration);
